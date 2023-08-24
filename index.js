8261f643b40bb489c8b5ffde1ad5bde460e82877
reniel905 <baldove.reniel905@gmail.com>
2023-08-24 11:25:13 +0800

    added hints

--- index.js
+++ index.js
@@ -33,8 +33,11 @@ function reRoll() {
     randomNumber = Math.floor((Math.random() * range) + 1);
     console.log(randomNumber);
     numberHolder.innerHTML = '?';
-    attemptHolder.innerHTML = ' Lives Left: ' + attemptCounter;
+    attemptHolder.innerHTML = 'Chance/s Left: ' + attemptCounter;
     guessButton.disabled = false;
+    reroll.style.display = "none";
+    userGuess.value = "";
+
 
 }
 
@@ -44,7 +47,7 @@ range1.addEventListener('click', function () {
     gameUI.style.display = "flex";
     mainMenu.style.display = "none";
 
-    attemptCounter = 1;
+    attemptCounter = 2;
     range = 10;
 
     reRoll();
@@ -59,26 +62,39 @@ range1.addEventListener('click', function () {
             scoreHolder.innerHTML = 'Score: ' + score;
             guessButton.disabled = true;
             reroll.style.display = "contents";
+            userGuess.value = "";
 
-        } else {
+        } else if (randomNumber > userGuess.value) {
 
             attemptCounter--;
-            attemptHolder.innerHTML = 'Lives Left: ' + attemptCounter;
+            attemptHolder.innerHTML = "Chance/s Left: " + attemptCounter;
+            reroll.style.display = "none";
+            window.alert('hint: go higher');
+            userGuess.value = "";
 
-            if (attemptCounter == 0) {
+        } else if (randomNumber < userGuess.value) {
 
-                numberHolder.innerHTML = randomNumber;
-                guessButton.disabled = true;
-                reroll.style.display = "none";
+            attemptCounter--;
+            attemptHolder.innerHTML = "Chance/s Left: " + attemptCounter;
+            reroll.style.display = "none";
+            window.alert('hint: go lower');
+            userGuess.value = "";
 
-            }
+        }
+
+        if (attemptCounter == 0) {
+
+            numberHolder.innerHTML = randomNumber;
+            guessButton.disabled = true;
+            reroll.style.display = "none";
+            userGuess.value = "";
 
         }
 
     });
 });
 
-range2.addEventListener('click', function () { 
+range2.addEventListener('click', function () {
 
     gameUI.style.display = "flex";
     mainMenu.style.display = "none";
@@ -98,25 +114,37 @@ range2.addEventListener('click', function () {
             scoreHolder.innerHTML = 'Score: ' + score;
             guessButton.disabled = true;
             reroll.style.display = "contents";
+            userGuess.value = "";
 
-        } else {
+        } else if (randomNumber > userGuess.value) {
 
             attemptCounter--;
-            attemptHolder.innerHTML = 'Lives Left: ' + attemptCounter;
+            attemptHolder.innerHTML = "Chance/s Left: " + attemptCounter;
             reroll.style.display = "none";
+            window.alert('hint: go higher');
+            userGuess.value = "";
 
-            if (attemptCounter == 0) {
+        } else if (randomNumber < userGuess.value) {
 
-                numberHolder.innerHTML = randomNumber;
-                guessButton.disabled = true;
-                reroll.style.display = "none";
+            attemptCounter--;
+            attemptHolder.innerHTML = "Chance/s Left: " + attemptCounter;
+            reroll.style.display = "none";
+            window.alert('hint: go lower');
+            userGuess.value = "";
 
-            }
+        }
+
+        if (attemptCounter == 0) {
+
+            numberHolder.innerHTML = randomNumber;
+            guessButton.disabled = true;
+            reroll.style.display = "none";
+            userGuess.value = "";
 
         }
 
     });
-    
+
 });
 range3.addEventListener('click', function () {
 
@@ -138,20 +166,32 @@ range3.addEventListener('click', function () {
             scoreHolder.innerHTML = 'Score: ' + score;
             guessButton.disabled = true;
             reroll.style.display = "contents";
+            userGuess.value = "";
 
-        } else {
+        } else if (randomNumber > userGuess.value) {
 
             attemptCounter--;
-            attemptHolder.innerHTML = 'Lives Left: ' + attemptCounter;
+            attemptHolder.innerHTML = "Chance/s Left: " + attemptCounter;
             reroll.style.display = "none";
+            window.alert('hint: go higher');
+            userGuess.value = "";
 
-            if (attemptCounter == 0) {
+        } else if (randomNumber < userGuess.value) {
 
-                numberHolder.innerHTML = randomNumber;
-                guessButton.disabled = true;
-                reroll.style.display = "none";
+            attemptCounter--;
+            attemptHolder.innerHTML = "Chance/s Left: " + attemptCounter;
+            reroll.style.display = "none";
+            window.alert('hint: go lower');
+            userGuess.value = "";
 
-            }
+        }
+
+        if (attemptCounter == 0) {
+
+            numberHolder.innerHTML = randomNumber;
+            guessButton.disabled = true;
+            reroll.style.display = "none";
+            userGuess.value = "";
 
         }
 
